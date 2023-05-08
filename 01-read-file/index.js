afcf7beb9dc81ae5fecc54023eb71a1685946bca
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-05-08 15:08:19 +0300

    feat: add first task solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,13 @@
+
+const path = require('path');
+const textPath = (path.join(__dirname, 'text.txt'))
+
+
+const fs = require('fs');
+const stream = fs.createReadStream(textPath, 'utf-8');
+
+let data = '';
+
+stream.on('data', (chunk) => (data += chunk));
+stream.on('end', () => console.log(data));
+stream.on('error', (error) => console.log('Error', error.message));
